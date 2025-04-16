e5c0ad2fb3b3b577b359f8807914df90e18ec3a8
Sichovuk <mykhailoyanki@gmail.com>
2025-04-16 17:24:09 +0300

    first commit

---
+++ fMain.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace laba_6
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbBasis = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbRadius = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbVPyr = new System.Windows.Forms.TextBox();
+            this.tbVSph = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbHeight);
+            this.groupBox1.Controls.Add(this.tbBasis);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(338, 129);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Дані піраміди";
+            // 
+            // tbBasis
+            // 
+            this.tbBasis.Location = new System.Drawing.Point(185, 39);
+            this.tbBasis.Name = "tbBasis";
+            this.tbBasis.Size = new System.Drawing.Size(129, 29);
+            this.tbBasis.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Висота";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Сторона основи";
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Location = new System.Drawing.Point(185, 79);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(129, 29);
+            this.tbHeight.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbRadius);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(370, 36);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(251, 94);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Дані сфери";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Радіус";
+            // 
+            // tbRadius
+            // 
+            this.tbRadius.Location = new System.Drawing.Point(97, 38);
+            this.tbRadius.Name = "tbRadius";
+            this.tbRadius.Size = new System.Drawing.Size(129, 29);
+            this.tbRadius.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbVSph);
+            this.groupBox3.Controls.Add(this.tbVPyr);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(37, 157);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(289, 140);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Об\'єми фігур";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "V піраміди";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "V сфери";
+            // 
+            // tbVPyr
+            // 
+            this.tbVPyr.Location = new System.Drawing.Point(129, 39);
+            this.tbVPyr.Name = "tbVPyr";
+            this.tbVPyr.ReadOnly = true;
+            this.tbVPyr.Size = new System.Drawing.Size(129, 29);
+            this.tbVPyr.TabIndex = 5;
+            // 
+            // tbVSph
+            // 
+            this.tbVSph.Location = new System.Drawing.Point(129, 85);
+            this.tbVSph.Name = "tbVSph";
+            this.tbVSph.ReadOnly = true;
+            this.tbVSph.Size = new System.Drawing.Size(129, 29);
+            this.tbVSph.TabIndex = 6;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(380, 196);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(241, 75);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Обрахувати об\'єми фігур";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click_1);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 331);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №6";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbBasis;
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbRadius;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbVSph;
+        private System.Windows.Forms.TextBox tbVPyr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ fMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace laba_6
+{
+    public partial class fMain : Form
+    {
+        public abstract class Solid
+        {
+            public abstract double GetVolume();
+        }
+
+        public class Pyramid : Solid
+        {
+            public double A { get; set; }
+            public double H { get; set; }
+
+            public Pyramid(double a, double h)
+            {
+                A = a;
+                H = h;
+            }
+
+            public override double GetVolume()
+            {
+                return (1.0 / 3.0) * A * A * H;
+            }
+        }
+
+        public class Sphere : Solid
+        {
+            public double R { get; set; }
+
+            public Sphere(double r)
+            {
+                R = r;
+            }
+
+            public override double GetVolume()
+            {
+                return (4.0 / 3.0) * Math.PI * Math.Pow(R, 3);
+            }
+        }
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                double basis = Convert.ToDouble(tbBasis.Text);
+                double height = Convert.ToDouble(tbHeight.Text);
+                double radius = Convert.ToDouble(tbRadius.Text);
+
+                Pyramid pyramid = new Pyramid(basis, height);
+                Sphere sphere = new Sphere(radius);
+
+                double pyramidVolume = pyramid.GetVolume();
+                double sphereVolume = sphere.GetVolume();
+
+                tbVPyr.Text = pyramidVolume.ToString("F2");
+                tbVSph.Text = sphereVolume.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Будь ласка, введіть коректні числові значення.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
